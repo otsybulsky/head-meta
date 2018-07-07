2f21e44b18223143763ecc5c51bef77e74c172a6
otsybulsky <helg37@gmail.com>
2018-07-07 12:04:39 +0300

    server template

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log('Tempate is ok')
+import http from 'http'
+import url from 'url'
+
+const server = new http.Server()
+
+server.on('request', function(req, res) {
+  const {
+    query: { url: targetUrl }
+  } = url.parse(req.url, true)
+
+  if (targetUrl) {
+    res.end(
+      JSON.stringify({
+        status: 'ok',
+        targetUrl: targetUrl
+      })
+    )
+  } else {
+    res.end(
+      JSON.stringify({
+        status: 'error',
+        reason: 'Url not finded in request params.'
+      })
+    )
+  }
+})
+
+server.listen(4001, 'localhost')
+console.log('server is started')
